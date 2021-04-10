228b3f7d864fe845644da14f307204ece830e27c
Daniel Lazar <lazardani0112@gmail.com>
2021-04-10 13:36:11 +0200

    Windows fixes.

--- public/electron.js
+++ public/electron.js
@@ -52,41 +52,55 @@ function createWindow() {
    //    win.webContents.openDevTools();
    // }
 
-   win.hide();
-   win.setVisibleOnAllWorkspaces(true);
+   switch (currentPlatform) {
+      case 'MAC': {
+
+         win.hide();
+         win.setVisibleOnAllWorkspaces(true);
+
+         trayIcon = isDev 
+         ? new Tray(`${path.join(__dirname, "../public/images/icon@1x.png")}`)
+         : new Tray(`${path.join(__dirname, "../build/images/icon@1x.png")}`);
+
+         // distinction between dev and prod in tray
+         if (isDev) {
+            trayIcon.setTitle('Dev');
+         }
+         trayIcon.setToolTip('Ducking Clipboard Manager');
+
+         // treating app showing and hiding on clicking trayIcon
+         trayIcon.on('click', () => {
+            if (browserWindowHidden) {
+               win.setPosition(trayIcon.getBounds().x, 0);
+               win.show();
+            } else {
+               win.hide();
+            }
+            browserWindowHidden = !browserWindowHidden;
+         });
+
+         // close the window if you click out of the app
+         win.on('blur', () => {
+            win.hide();
+            browserWindowHidden = true;
+         });
+
+         break;
+      }
+      case 'WINDOWS' || 'LINUX': {
+         break;
+      }
+      default: {
+         console.error('You are using an unsupported OS!');
+         break;
+      }
+   }
 
    win.loadURL(
       isDev
          ? "http://localhost:3000"
          : `file:///${__dirname}/../build/index.html`
    );
-
-   trayIcon = isDev 
-   ? new Tray(`${path.join(__dirname, "../public/images/icon@1x.png")}`)
-   : new Tray(`${path.join(__dirname, "../build/images/icon@1x.png")}`);
-
-   // distinction between dev and prod in tray
-   if (isDev) {
-      trayIcon.setTitle('Dev');
-   }
-   trayIcon.setToolTip('Ducking Clipboard Manager');
-
-   // treating app showing and hiding on clicking trayIcon
-   trayIcon.on('click', () => {
-      if (browserWindowHidden) {
-         win.setPosition(trayIcon.getBounds().x, 0);
-         win.show();
-      } else {
-         win.hide();
-      }
-      browserWindowHidden = !browserWindowHidden;
-   });
-
-   // close the window if you click out of the app
-   win.on('blur', () => {
-      win.hide();
-      browserWindowHidden = true;
-   });
 }
 
 app.whenReady().then(() => {
